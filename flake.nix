ca12e181fc4c41b52e30df90c5702510630f9e8a
William Fish <william.michael.fish@gmail.com>
2025-07-17 12:52:02 +0100

    Install with flake

--- flake.nix
+++ flake.nix
@@ -4,29 +4,47 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
       let
         pkgs = import nixpkgs { system = system; };
 
         lint = pkgs.writeScriptBin "lint" ''
           pre-commit run --all-files --show-diff-on-failure
         '';
-        update-modules = pkgs.writeScriptBin "update-modules" ''
-          cd collector
-          go get -u ./...
-        '';
-      in {
+        sniffy = pkgs.buildGoModule {
+          pname = "sniffy";
+          version = "0.1.0";
+
+          src = ./.;
+
+          vendorHash = "sha256-5HhG2GAvf6COM4qN0YZU6kQDiXTcrkjj7Itfee2vK6E=";
+
+          meta = with pkgs.lib; {
+            description = "A tool for finding unused secrets";
+            homepage = "https://github.com/willfish/sniffy";
+            license = licenses.mit;
+            maintainers = [ maintainers.willfish ];
+          };
+        };
+      in
+      {
+        packages.default = sniffy;
+        packages.sniffy = sniffy;
         devShells.default = pkgs.mkShell {
           buildInputs = with pkgs; [
-            circleci-cli
             go
             golangci-lint
             gopls
             lint
-            serverless
-            update-modules
           ];
         };
-      });
+      }
+    );
 }
