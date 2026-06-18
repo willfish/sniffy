80c181aeb877ece9e828bd7c190a72b866e0891d
William Fish <william.michael.fish@gmail.com>
2026-06-18 16:48:50 +0100

    chore: manage pre-commit hooks with Nix
    Issue: No ticket/issue

--- flake.nix
+++ flake.nix
@@ -2,13 +2,17 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    pre-commit-hooks = {
+      url = "github:cachix/git-hooks.nix";
+    };
   };
 
   outputs =
     {
-      self,
       nixpkgs,
       flake-utils,
+      pre-commit-hooks,
+      ...
     }:
     flake-utils.lib.eachDefaultSystem (
       system:
@@ -33,17 +37,115 @@
             maintainers = [ maintainers.willfish ];
           };
         };
+
+        preCommitCheck = pre-commit-hooks.lib.${system}.run {
+          src = ./.;
+          configPath = ".pre-commit-config-nix.yaml";
+          default_stages = [ "pre-commit" ];
+          hooks = {
+            actionlint = {
+              enable = true;
+              stages = [ "pre-commit" ];
+            };
+            check-added-large-files = {
+              enable = true;
+              stages = [ "pre-commit" ];
+            };
+            check-case-conflicts = {
+              enable = true;
+              stages = [ "pre-commit" ];
+            };
+            check-executables-have-shebangs = {
+              enable = true;
+              stages = [ "pre-commit" ];
+            };
+            check-json = {
+              enable = true;
+              stages = [ "pre-commit" ];
+            };
+            check-merge-conflicts = {
+              enable = true;
+              stages = [ "pre-commit" ];
+            };
+            check-shebang-scripts-are-executable = {
+              enable = true;
+              stages = [ "pre-commit" ];
+            };
+            check-toml = {
+              enable = true;
+              stages = [ "pre-commit" ];
+            };
+            check-yaml = {
+              enable = true;
+              stages = [ "pre-commit" ];
+            };
+            deadnix = {
+              enable = true;
+              stages = [ "pre-commit" ];
+            };
+            end-of-file-fixer = {
+              enable = true;
+              stages = [ "pre-commit" ];
+            };
+            mixed-line-endings = {
+              enable = true;
+              stages = [ "pre-commit" ];
+            };
+            nixfmt-rfc-style = {
+              package = pre-commit-hooks.inputs.nixpkgs.legacyPackages.${system}.nixfmt;
+              enable = true;
+              stages = [ "pre-commit" ];
+            };
+            shellcheck = {
+              enable = true;
+              stages = [ "pre-commit" ];
+            };
+            trim-trailing-whitespace = {
+              enable = true;
+              stages = [ "pre-commit" ];
+            };
+          };
+        };
       in
       {
         packages.default = sniffy;
         packages.sniffy = sniffy;
         devShells.default = pkgs.mkShell {
-          buildInputs = with pkgs; [
-            go
-            golangci-lint
-            gopls
-            lint
-          ];
+          shellHook = ''
+            ${preCommitCheck.shellHook}
+            export PATH=${pkgs.writeShellScriptBin "pre-commit" ''
+              set -euo pipefail
+
+              has_config=false
+              for arg in "$@"; do
+                case "$arg" in
+                  -c|--config|--config=*)
+                    has_config=true
+                    ;;
+                esac
+              done
+
+              if [ "$has_config" = true ]; then
+                exec ${preCommitCheck.config.package}/bin/pre-commit "$@"
+              fi
+
+              if [ "''${1:-}" = "run" ]; then
+                shift
+                exec ${preCommitCheck.config.package}/bin/pre-commit run --config .pre-commit-config-nix.yaml "$@"
+              fi
+
+              exec ${preCommitCheck.config.package}/bin/pre-commit "$@"
+            ''}/bin:$PATH
+          '';
+
+          buildInputs =
+            preCommitCheck.enabledPackages
+            ++ (with pkgs; [
+              go
+              golangci-lint
+              gopls
+              lint
+            ]);
         };
       }
     );
